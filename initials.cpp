b253532acf73074c78bb6345f34e323a7e28d78b
mini-ray <55558121+mini-ray@users.noreply.github.com>
2021-03-30 11:25:04 -0700

    Add files via upload

---
+++ initials.cpp
@@ -0,0 +1,15 @@
+// g++ initials.cpp -o cwc.o
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << " sss  w                   w  sss \n";
+  cout << "s   s  w                 w  s   s\n";
+  cout << "s       w       w       w   s    \n";
+  cout << " sss     w     w w     w     sss \n";
+  cout << "    s     w   w   w   w         s\n";
+  cout << "s   s      w w     w w      s   s\n";
+  cout << " sss        w       w        sss \n";
+  
+  return 0;
+}
